87ba5780b75137e7585b633adbccf8873e524e57
NickMoo5 <nicholasamah@gmail.com>
2026-07-21 18:41:33 -0700

    Add delay after eval

--- main.py
+++ main.py
@@ -116,6 +116,7 @@ class mainProcess:
                 print("[SYS] Birdie detected! Starting evaluation")
                 time.sleep(0.5)  # give the birdie a moment to settle before evaluation
                 self.qcm.run_evaluation()
+                time.sleep(0.7)  # give the birdie a moment to leave before resuming READY
 
                 print("[SYS] Evaluation complete, returning to READY")
 
